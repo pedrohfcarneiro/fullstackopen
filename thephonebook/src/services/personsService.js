e426ea11ca6adeb694ee15b2c1c1d5a195fe1e41
pedrohfcarneiro <phtriplox8090@gmail.com>
2022-04-06 15:39:28 -0300

    changed to use backend on heroku

--- thephonebook/src/services/personsService.js
+++ thephonebook/src/services/personsService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://salty-fjord-97682.herokuapp.com/phonebookapi/contacts'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
